230ec5f5fc97aa5cf7ae380353b8fc1b53890bb0
Cole Crawford <cole_crawford@harvard.edu>
2020-01-15 16:00:10 -0500

    Fixing CSS, add tooltip

--- static/js/colorize.js
+++ static/js/colorize.js
@@ -16,8 +16,8 @@ $( document ).ready( function() {
 	$('body').on('pageLoadComplete',function() {
 		var url = window.location.href;
 		var pageSlug = window.location.pathname.split("/").pop();
-		console.log(url);
-		console.log(pageSlug);
+		// console.log(url);
+		// console.log(pageSlug);
 		let colorize = true;
     createToggleButton(colorize);
     getScalarNode(pageSlug, processHtml);
@@ -26,10 +26,10 @@ $( document ).ready( function() {
 			console.log("getting scalar node");
 			// var scalar_api_json_uri = pageUri + ".rdfjson";
 			var scalar_api_json_uri = url + ".rdfjson";
-			console.log(scalar_api_json_uri);
+			// console.log(scalar_api_json_uri);
 			$.getJSON(scalar_api_json_uri, function(data){
-				console.log("Got Scalar data!");
-				console.log(data);
+				// console.log("Got Scalar data!");
+				// console.log(data);
 				let latest = data[url]["http://scalar.usc.edu/2012/01/scalar-ns#version"][0].value;
 				let node = data[latest];
 				window.raw_content = node["http://rdfs.org/sioc/ns#content"][0].value;
@@ -40,8 +40,8 @@ $( document ).ready( function() {
 		function processHtml(content){
 			console.log("process html");
 			colorizehtml(raw_content).then(function(response){
-				console.log("colorized");
-				console.log(response);
+				// console.log("colorized");
+				// console.log(response);
         window.colorized_content = response;
 				if(colorize){
 					$( "span[property='sioc:content']" ).html(colorized_content);
@@ -53,21 +53,30 @@ $( document ).ready( function() {
 
     function createToggleButton(colorize_text=true){
       var ru_toggle = `<img class="ru-toggle" title="" data-toggle="popover" data-colorize="${colorize_text}"data-placement="bottom" src="https://harvard-atg.github.io/scalar-assets/static/img/ru_flag_round_250.png" alt="Russian colorize toggle">`;
+			var tooltip = `<div class="popover caption_font fade left in" role="tooltip" id="colorize_tooltip" style="">Toggle Colorization (Word Levels)</div>`
       $("body").append(ru_toggle);
+			$(tooltip).insertAfter(".ru-toggle");
       $(".ru-toggle").click(toggleColorization);
+			$(".ru-toggle").mouseover(function(){
+				$("#tooltip").toggle();
+			})
+			$(".ru-toggle").mouseout(function(){
+				$("#tooltip").toggle();
+			})
     }
 
 		function toggleColorization(){
-      console.log("colorize toggled");
-			console.log(this);
+      console.log("Colorize toggled");
       if($(this).data("colorize") == true){
         $(this).data("colorize", false);
 				colorize = false;
         $( "span[property='sioc:content']" ).html(raw_content);
+				$(".ru-toggle").css({"opacity": ".25"});
       } else {
         $(this).data("colorize", true);
 				colorize = true;
         $( "span[property='sioc:content']" ).html(colorized_content);
+				$(".ru-toggle").css({"opacity": ".75"});
       }
 		}
 
